14ae86b7ff71653ab4b7cf86463c024be158ae0e
Anibal Cruz <cruz@zoocru.com>
2022-02-28 12:52:48 -0500

    updated db file

--- public/db.php
+++ public/db.php
@@ -4,10 +4,10 @@
 	//$con = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
 
 	// Production connection
-	//$con = mysqli_connect("localhost","payweb1_db","6Sz2gwJ13jw]","payweb1_db");
+	$con = mysqli_connect("localhost","payweb1_db","6Sz2gwJ13jw]","payweb1_db");
 	
 	// Local connection
-	$con = mysqli_connect("localhost:8889","root","cruz","payrailz");
+	//$con = mysqli_connect("localhost:8889","root","cruz","payrailz");
 
 	// Check connection
 	if (mysqli_connect_errno()) {
